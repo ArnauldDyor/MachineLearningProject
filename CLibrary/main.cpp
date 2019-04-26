c80c91f669888b6a0bf66b267eddf77883cc526e
VictorMeyer77 <44139012+VictorMeyer77@users.noreply.github.com>
2019-04-27 00:06:38 +0200

    Add files via upload

--- CLibrary/main.cpp
+++ CLibrary/main.cpp
@@ -1,16 +1,17 @@
 #include "main.h"
 
+// NE PAS HESITER A CORRIGER OU COMPARER AVEC DES CONNAISSANCES, JE NE SUI SUR DE RIEN
+// TROUVER UN MOYEN DE RECUP LES W DANS LE CODE PYTHON SERAIT BIENVENUE
 
 extern "C"{
 
+    SUPEREXPORT double* create_linear_model(int sampleCount){
 
-    SUPEREXPORT double* create_linear_model(int inputCountPerSample){
-
-        double* W = new double[inputCountPerSample + 1];
+        double* W = new double[sampleCount + 1];
         const int POINT = 100;
         std::srand(std::time(0));
 
-        for (int i = 0; i < inputCountPerSample + 1; i++) {
+        for (int i = 0; i < sampleCount + 1; i++) {
 
             double randVal = std::rand() % POINT;
             int powerCount = std::rand() % POINT;
@@ -25,6 +26,7 @@ extern "C"{
         return W;
     }
 
+    // A TESTER ET CORRIGER, GROS DOUTES
     SUPEREXPORT void fit_classification_rosenblatt_rule(
             double* W,
             double* XTrain,
@@ -37,12 +39,14 @@ extern "C"{
         {
             for (int i = 0; i < epochs; i++)
             {
-                for (int k = 0; k < inputCountPerSample; k++)
+                for (int k = 0; k < sampleCount; k++)
                 {
-                    W[i] = W[i] + alpha * (YTrain[k] /* - ?? */) * XTrain[k];
+                    W[i] = W[i] + alpha * (YTrain[k]  - predict_classification(W, XTrain, sampleCount, inputCountPerSample)) * XTrain[k];
                 }
             }
         }
+
+    // SEMBLE BIEN FONCTIONNER
 
     SUPEREXPORT void fit_regression(
 		double* W,
@@ -52,53 +56,97 @@ extern "C"{
 		double* YTrain
 	)
 	{
-	    MatrixXd xMat(inputCountPerSample, 1);
-	    MatrixXd yMat(inputCountPerSample, 1);
+	    MatrixXd xMat = translateTrainingData(XTrain, sampleCount, inputCountPerSample);
+	    xMat = addBias(xMat);
+	    MatrixXd yMat(sampleCount, 1);
 
-        for(int i = 0; i < inputCountPerSample; i ++){
+        for(int i = 0; i < sampleCount; i ++){
             yMat.row(i).col(0) << YTrain[i];
         }
-        for(int i = 0; i < inputCountPerSample; i ++){
-            xMat.row(i).col(0) << XTrain[i];
-        }
-
 
         MatrixXd expr = xMat.transpose() * xMat;
         expr = expr.inverse();
         MatrixXd result = (expr * xMat.transpose()) * yMat;
 
-        for(int i = 0; i < inputCountPerSample; i ++){
+        // SORTIE TEMPORAIRE, POUR VOIRE RAPIDEMENT LES POIDS
+        cout << result;
+
+        for(int i = 0; i < result.cols(); i ++){
             W[i] = result(i);
         }
 
 	}
 
+    // SEMBLE FONCTIONNER MAIS A QUOI SERT - ELLE ??
 	SUPEREXPORT double predict_regression(
 		double* W,
 		double* XToPredict,
+		int sampleCount,
 		int inputCountPerSample
 	)
 	{
-	    double predict_coeff = 0;                    //inference bloc
-	    for(int i = 0; i < inputCountPerSample; i ++){
-            predict_coeff += W[i] * XToPredict[i];
-	    }
-		return predict_coeff;
+		MatrixXd xMat = translateTrainingData(XToPredict, sampleCount, inputCountPerSample);
+		xMat = addBias(xMat);
+
+		MatrixXd wMat = translateTrainingData(W, sampleCount, 1);
+
+		MatrixXd result = wMat.transpose() * xMat;
+
+		double coef_predict = result(0, 0);
+
+		return coef_predict < 0 ? -1.0 : 1.0;
+
 	}
 
 	SUPEREXPORT double predict_classification(
 		double* W,
 		double* XToPredict,
-		int inputCountPerSample
+		int inputCountPerSample,
+		int sampleCount
 	)
 	{
-		return predict_regression(W, XToPredict, inputCountPerSample) >= 0 ? 1.0 : -1.0;
+		return predict_regression(W, XToPredict, sampleCount, inputCountPerSample) >= 0 ? 1.0 : -1.0;
 	}
 
 	SUPEREXPORT void delete_linear_model(double* W)
 	{
 		delete[] W;
 	}
+
+	// FONCTIONS INTERNES //
+
+    // TRANSFORME UN TABLEAU EN MATRICE
+	SUPEREXPORT MatrixXd translateTrainingData(double* XTrain,  int sampleCount, int inputCountPerSample){
+
+        MatrixXd xMat(sampleCount, inputCountPerSample);
+        int cursor = 0;
+
+        for(int i = 0; i < sampleCount; i ++){
+
+            for(int j = 0; j < inputCountPerSample; j ++){
+
+                xMat.row(i).col(j) << XTrain[cursor];
+                cursor += 1;
+            }
+
+        }
+
+        return xMat;
+
+	}
+
+	// AJOUTE BIAIS
+	SUPEREXPORT MatrixXd addBias(MatrixXd xMat){
+
+        xMat.conservativeResize(xMat.rows(), xMat.cols() + 1);
+
+        for(int i = 0; i < xMat.rows(); i ++){
+            xMat.row(i).col(xMat.cols() - 1) << 1;
+        }
+
+        return xMat;
+	}
+
 
 }
 
--- CLibrary/main.h
+++ CLibrary/main.h
@@ -14,3 +14,13 @@
 using namespace Eigen;
 using namespace std;
 
+extern "C" {
+    SUPEREXPORT double* create_linear_model(int inputCountPerSample);
+    SUPEREXPORT void fit_classification_rosenblatt_rule(double* W, double* XTrain, int sampleCount, int inputCountPerSample, double* YTrain, double alpha, int epochs);
+    SUPEREXPORT void fit_regression(double* W, double* XTrain, int sampleCount, int inputCountPerSample, double* YTrain);
+    SUPEREXPORT double predict_regression(double* W, double* XToPredict, int sampleCount, int inputCountPerSample);
+    SUPEREXPORT double predict_classification(double* W, double* XToPredict,int sampleCount, int inputCountPerSample);
+    SUPEREXPORT void delete_linear_model(double* W);
+    SUPEREXPORT MatrixXd translateTrainingData(double* XTrain,  int sampleCount, int inputCountPerSample);
+    SUPEREXPORT MatrixXd addBias(MatrixXd xMat);
+}
