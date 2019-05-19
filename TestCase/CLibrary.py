e98cb42b216ad9caed797cbcabfa0a4fbe3f04f3
VictorMeyer77 <44139012+VictorMeyer77@users.noreply.github.com>
2019-05-19 20:22:52 +0200

    Add files via upload

--- TestCase/CLibrary.py
+++ TestCase/CLibrary.py
@@ -1,48 +1,60 @@
 from ctypes import *
+import numpy as np
 
-myDll = CDLL("C:/Users/utilisateur/Desktop/Projet Annuel/testLibC/bin/Release/testLibC.dll")
+myDll = CDLL("/home/victor/Programmation/C/Shared/main.so")
 
 
 def create_linear_model(sampleCount):
-
     myDll.create_linear_model.argtypes = [c_int32]
     myDll.create_linear_model.restype = POINTER(ARRAY(c_double, sampleCount))
     weight = myDll.create_linear_model(sampleCount)
     return weight
 
-def fit_classification_rosenblatt_rule(W, XTrain, sampleCount, inputCountPerSample, YTrain, alpha, epochs):
 
-    WPointer = (c_double * len(W))(*W)
+def fit_classification_rosenblatt_rule(W, XTrain, sampleCount, inputCountPerSample, inputCountPerResult, YTrain, alpha,
+                                       epochs):
+
+    # configuration du fichier Model.txt
+    configureModelFile(inputCountPerResult, inputCountPerSample + 1, 67)
+
     XTrainPointer = (c_double * len(XTrain))(*XTrain)
     YTrainPointer = (c_double * len(YTrain))(*YTrain)
 
-    myDll.fit_classification_rosenblatt_rule.argtypes = [POINTER(ARRAY(c_double, len(W))), POINTER(ARRAY(c_double, len(YTrain))),
-                                                         c_int32, c_int32, POINTER(ARRAY(c_double, len(XTrain))), c_double, c_int32]
+    myDll.fit_classification_rosenblatt_rule.argtypes = [POINTER(ARRAY(c_double, inputCountPerSample)),
+                                                         POINTER(ARRAY(c_double, len(XTrain))),
+                                                         c_int32, c_int32, c_int32,
+                                                         POINTER(ARRAY(c_double, len(YTrain))), c_double, c_int32]
     myDll.fit_classification_rosenblatt_rule.restype = c_void_p
-    myDll.fit_classification_rosenblatt_rule(WPointer, XTrainPointer, sampleCount, inputCountPerSample, YTrainPointer, alpha, epochs)
+    myDll.fit_classification_rosenblatt_rule(W, XTrainPointer, sampleCount, inputCountPerSample, inputCountPerResult,
+                                             YTrainPointer, alpha, epochs)
+
 
 def fit_regression(WPointer, XTrain, sampleCount, inputCountPerSample, YTrain):
 
+    # configuration du fichier Model.txt
+    configureModelFile(0, inputCountPerSample, 82)
+
     XTrainPointer = (c_double * len(XTrain))(*XTrain)
     YTrainPointer = (c_double * len(YTrain))(*YTrain)
 
-    myDll.fit_regression.argtypes = [POINTER(ARRAY(c_double, inputCountPerSample + 1)), POINTER(ARRAY(c_double, len(XTrain))),
+    myDll.fit_regression.argtypes = [POINTER(ARRAY(c_double, inputCountPerSample + 1)),
+                                     POINTER(ARRAY(c_double, len(XTrain))),
                                      c_int32, c_int32, POINTER(ARRAY(c_double, len(YTrain)))]
     myDll.fit_regression.restype = c_void_p
     myDll.fit_regression(WPointer, XTrainPointer, sampleCount, inputCountPerSample, YTrainPointer)
 
 
-def predict_regression(WPointer, XTrain, sampleCount, inputCountPerSample):
-
+def predict_regression(WPointer, XTrain, inputCountPerSample):
     XTrainPointer = (c_double * len(XTrain))(*XTrain)
 
-    myDll.predict_regression.argtypes = [POINTER(ARRAY(c_double,  inputCountPerSample + 1)), POINTER(ARRAY(c_double, len(XTrain))), c_int32, c_int32]
+    myDll.predict_regression.argtypes = [POINTER(ARRAY(c_double, inputCountPerSample + 1)),
+                                         POINTER(ARRAY(c_double, len(XTrain))), c_int32]
     myDll.predict_regression.restype = c_double
-    coef = myDll.predict_regression(WPointer, XTrainPointer, sampleCount, inputCountPerSample)
+    coef = myDll.predict_regression(WPointer, XTrainPointer, inputCountPerSample)
     return coef
 
-def predict_classification(W, XTrain, sampleCount, inputCountPerSample):
 
+def predict_classification(W, XTrain, sampleCount, inputCountPerSample):
     WPointer = (c_double * len(W))(*W)
     XTrainPointer = (c_double * len(XTrain))(*XTrain)
 
@@ -53,19 +65,42 @@ def predict_classification(W, XTrain, sampleCount, inputCountPerSample):
     coef = myDll.predict_classification(WPointer, XTrainPointer, sampleCount, inputCountPerSample)
     return coef
 
-def delete_linear_model(W):
 
+def delete_linear_model(W):
     WPointer = (c_double * len(W))(*W)
 
     myDll.delete_linear_model.argtype = POINTER(ARRAY(c_double, len(W)))
     myDll.delete_linear_model.restype = c_void_p
     myDll.delete_linear_model(WPointer)
 
-def outPointer(pointer, size):
 
-    myDll.outPointer.argtypes = [POINTER(ARRAY(c_double, size)), c_int32]
-    myDll.outPointer.restype = c_void_p
-    myDll.outPointer(pointer, size)
+def useTrainModel(X):
+    XPointer = (c_double * len(X))(*X)
+    myDll.useTrainModel.argtype = POINTER(ARRAY(c_double, len(X)))
+    myDll.useTrainModel.restype = c_double
+    res = myDll.useTrainModel(XPointer)
+    return res
 
 
+def configureModelFile(inputCountPerResult, inputCountPerSample, modele):
+    myDll.configureModelFile.argtype = [c_int32, c_int32, c_char]
 
+    myDll.configureModelFile.restype = c_void_p
+    myDll.configureModelFile(inputCountPerResult, inputCountPerSample, modele)
+
+
+### FONCTIONS PUR PYHTON
+
+
+def toArray(X, sampleCount, inputCountPerSample):
+    X = X.tolist()
+    if (sampleCount == 1):
+        return X
+    Xarray = []
+
+    for i in range(0, sampleCount):
+
+        for j in range(0, inputCountPerSample):
+            Xarray.append(X[i][j])
+
+    return Xarray
