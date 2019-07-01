7f1ee2b46dc162edbd45881ad66a7e17ab4f8a5a
VictorMeyer77 <44139012+VictorMeyer77@users.noreply.github.com>
2019-07-01 18:15:07 +0200

    Add files via upload

--- Entrainement/Main.py
+++ Entrainement/Main.py
@@ -10,24 +10,25 @@ REP = ["/home/victor/esgi/L3/Projet2019/MachineLearningProject/Dataset/FR/",
 if __name__ == '__main__':
 
 
-
     # UNCOMENT TO TRAIN
     # TESTER EN DIVISANT PAR LE MAX (XTrain / np.max(XTrain)
-    #XTrain, YTrain = tm.create_train_mat(REP)
-    #tm.train_model(XTrain, YTrain, len(REP) - 1)
+    XTrain, YTrain = tm.create_train_mat(REP)
+    #rosenblatt
+    """"
+    tm.train_model(XTrain, YTrain, len(REP) - 1)"""
 
-
+    #mlp
+    tm.train_mlp(XTrain, YTrain, len(REP) - 1)
 
     #utilisation cas france
     os.chdir(REP[0])
     lt = os.listdir(REP[0])
     
-    for i in range(0, 50):
-        try :
+    for i in range(0, 2):
+        try:
             img = cv2.imread(lt[400 + i])
             img = cv2.resize(img, (100, 100))
 
-            # si division par le max, rajouter / np.max(XTrain)
-            tm.use_model(img)
+            tm.use_mlp(img)
         except Exception:
             print("error")
